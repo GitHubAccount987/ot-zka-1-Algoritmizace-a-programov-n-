31d667b3a7621336d03a4e95a2a1a50d6a66a85b
vojtech.vlcek <vojtech.vlcek@student.spsmb.cz>
2024-10-09 11:53:09 +0200

    Init

--- cv1/src/Main.java
+++ cv1/src/Main.java
@@ -1,6 +1,168 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 1.	Vytvořte program, kde uživatel zadá na vstupu do konzole číslo a
+ * program následně vypíše do konzole trojúhelník. Číslo slouží pro definování
+ * výšky trojúhelníku dle následující ukázky. (max 2b)
+ *
+ * Ukázka:
+ * Zadejte číslo: 3
+ * *
+ * **
+ * ***
+ * ****
+ * ***
+ * **
+ * *
+ *
+ *
+ *
+ * Ukázka:
+ * Zadejte číslo: 4
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ****
+ * ***
+ * 	**
+ * 	*
+ *
+ *
+ *
+ *2.	Vytvořte program, kde uživatel zadá na vstupu do konzole větu a program
+ * následně vypíše zadanou větu pozpátku. (max 1b)
+ *
+ * 3.	Vytvořte program, kde uživatel zadá na vstupu do konzole větu a program
+ * spočítá kolik slov věta obsahuje. (max 2b)
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Scanner scanner = new Scanner(System.in, "Windows-1250");
+
+        while (true) {
+
+            System.out.print("Vyberte úlohu:\n1 → úloha č. 1 (trojúhelník)\n2 → Úloha č. 2 (věta pozpátku)\n3 → Úloha č. 3 (počet slov)\n4 → Ukončit program\nMožnost: ");
+
+            int identifier;
+
+            try {
+
+                identifier = Integer.parseInt(scanner.nextLine());
+
+            } catch (Exception e) {
+
+                System.out.println("\nNe fakt chci číslo ne to co si mi napsal\n");
+
+                continue;
+            }
+
+            switch (identifier) {
+
+                case 1:
+                    assignment1();
+                    break;
+                case 2:
+                    assignment2();
+                    break;
+                case 3:
+                    assignment3();
+                    break;
+                case 4:
+                    return;
+                default:
+                    System.out.println("\nNeznámá možnost!\n");
+
+            }
+        }
+    }
+
+    public static void assignment1() {
+
+        System.out.println("-- // ASSIGNMENT 1 \\\\ --");
+
+        Scanner scanner = new Scanner(System.in, "Windows-1250");
+
+        int height;
+
+        while (true) {
+
+            System.out.print("Výška trojúhelníku: ");
+
+            try {
+
+                height = Integer.parseInt(scanner.nextLine());
+
+            } catch (Exception e) {
+
+                System.out.println("Kamo ti jsy negramotní? výška asy musí bít čýslo");
+
+                continue;
+            }
+
+            break;
+        }
+
+        if (height < 2) System.out.println("Výška musí být větší jak 1");
+
+        int accumulator = 1;
+        int increment = 1;
+
+        for (int i = 0; i < 2*height - 1; i++) {
+
+            for (int j = 0; j < accumulator; j++) System.out.print("*");
+
+            increment -= 2*Math.floorDiv(accumulator, height);
+
+            accumulator += increment;
+
+            System.out.println();
+
+        }
+
+        System.out.println("-- // END OF ASSIGNMENT 1 \\\\ --");
+    }
+
+    public static void assignment2() {
+
+        System.out.println("-- // ASSIGNMENT 2 \\\\ --");
+
+        System.out.print("zadejte větu: ");
+
+        Scanner scanner = new Scanner(System.in, "Windows-1250");
+
+        String sentence = scanner.nextLine();
+
+        for (int i = sentence.length() - 1; i >= 0; i--) System.out.print(sentence.charAt(i));
+
+        System.out.println("-- // END OF ASSIGNMENT 2 \\\\ --");
+    }
+
+    public static void assignment3() {
+
+        System.out.println("-- // ASSIGNMENT 3 \\\\ --");
+
+        System.out.print("Zadejte větu: ");
+
+        Scanner scanner = new Scanner(System.in, "Windows-1250");
+
+        String sentence = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("[^ ]+");
+        Matcher matcher = pattern.matcher(sentence);
+
+        int accumulator = 0;
+
+        while (matcher.find()) accumulator++;
+
+        System.out.println(accumulator);
+
+        System.out.println("-- // END OF ASSIGNMENT 3 \\\\ --");
     }
 }
