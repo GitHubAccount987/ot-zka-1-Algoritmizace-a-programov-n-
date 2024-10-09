4778829e7b79a7293b101fcbaff0c2fda5dc7028
vojtech.vlcek <vojtech.vlcek@student.spsmb.cz>
2024-10-09 11:58:53 +0200

    Tuning

--- cv1/src/Main.java
+++ cv1/src/Main.java
@@ -2,44 +2,6 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * 1.	Vytvořte program, kde uživatel zadá na vstupu do konzole číslo a
- * program následně vypíše do konzole trojúhelník. Číslo slouží pro definování
- * výšky trojúhelníku dle následující ukázky. (max 2b)
- *
- * Ukázka:
- * Zadejte číslo: 3
- * *
- * **
- * ***
- * ****
- * ***
- * **
- * *
- *
- *
- *
- * Ukázka:
- * Zadejte číslo: 4
- * *
- * **
- * ***
- * ****
- * *****
- * ****
- * ***
- * 	**
- * 	*
- *
- *
- *
- *2.	Vytvořte program, kde uživatel zadá na vstupu do konzole větu a program
- * následně vypíše zadanou větu pozpátku. (max 1b)
- *
- * 3.	Vytvořte program, kde uživatel zadá na vstupu do konzole větu a program
- * spočítá kolik slov věta obsahuje. (max 2b)
- */
-
 public class Main {
 
     public static void main(String[] args) {
